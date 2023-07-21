0ea0f91902de051c22c568cb666e782d1c7951fa
Jaben3 <benduch3@gmail.com>
2023-07-21 11:31:20 +0200

    Moved setup and teardown methods to BaseTest class Added headless mode

--- src/test/java/tests/PageObjectModelTest.java
+++ src/test/java/tests/PageObjectModelTest.java
@@ -1,34 +1,16 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class PageObjectModelTest {
+public class PageObjectModelTest extends BaseTest{
 
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setup(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
-
-    @AfterMethod
-    public void teardown(){
-        driver.quit();
-    }
 
     @Test
     public void TrialOfStonesTest(){
@@ -63,5 +45,4 @@ public class PageObjectModelTest {
         Assert.assertTrue(driver.findElement(By.id("trialCompleteBanner")).isDisplayed());
         Assert.assertEquals(driver.findElement(By.id("trialCompleteBanner")).getText(), "Trial Complete");
     }
-
 }
